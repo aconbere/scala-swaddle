3ae275b460d0e42bd6c51f00bd8fde29f3d11c64
Anders Conbere <aconbere@etsy.com>
2013-04-18 15:51:58 -0700

    first

---
+++ build.sbt
@@ -0,0 +1,27 @@
+name := "jacks"
+
+version := "0.1.0"
+
+organization := "org.conbere"
+
+licenses := Seq("BSD-style" -> url("http://www.opensource.org/licenses/bsd-license.php"))
+
+homepage := Some(url("http://github.com/aconbere/scala-irc"))
+
+scalaVersion := "2.10.0"
+
+scalacOptions ++= Seq(
+  "-deprecation",
+  "-feature"
+)
+
+fork in run := true
+
+resolvers += "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/"
+
+testOptions in Test += Tests.Argument("-oDF")
+
+libraryDependencies ++= Seq(
+  "com.fasterxml.jackson.module" %% "jackson-module-scala" % "2.2.0-rc1",
+  "com.fasterxml.jackson.core" % "jackson-databind" % "2.1.3"
+)
---
+++ src/main/scala/Swaddle.scala
@@ -0,0 +1,74 @@
+import com.fasterxml.jackson.databind.{ ObjectMapper, DeserializationFeature, SerializationFeature }
+import com.fasterxml.jackson.databind.cfg.ConfigFeature;
+import com.fasterxml.jackson.module.scala.DefaultScalaModule
+import com.fasterxml.jackson.core.JsonParser
+
+import java.lang.reflect.{Type, ParameterizedType}
+import com.fasterxml.jackson.core.`type`.TypeReference;
+
+trait JacksLike {
+  val serializationFeatures:List[(SerializationFeature, Boolean)]
+  val deserializationFeatures:List[(DeserializationFeature, Boolean)]
+  val parserFeatures:List[(JsonParser.Feature, Boolean)]
+
+  def deserialize[T: Manifest](value: String) : T =
+    mapper.readValue(value, typeReference[T])
+
+  lazy val mapper =  {
+    val m = new ObjectMapper()
+
+    m.registerModule(DefaultScalaModule)
+
+    for ((feature, flag) <- serializationFeatures) { 
+      m.configure(feature, flag)
+    }
+
+    for ((feature, flag) <- deserializationFeatures) { 
+      m.configure(feature, flag)
+    }
+
+    for ((feature, flag) <- parserFeatures) { 
+      m.configure(feature, flag)
+    }
+
+    m
+  }
+
+  private [this] def typeReference[T: Manifest] = new TypeReference[T] {
+    override def getType = typeFromManifest(manifest[T])
+  }
+
+  private [this] def typeFromManifest(m: Manifest[_]): Type = {
+      if (m.typeArguments.isEmpty) {
+        m.runtimeClass 
+      } else new ParameterizedType {
+        def getRawType = m.runtimeClass
+
+        def getActualTypeArguments = m.typeArguments.map(typeFromManifest).toArray
+
+        def getOwnerType = null
+      }
+  }
+}
+
+class Jacks (
+  val serializationFeatures:List[(SerializationFeature, Boolean)] = List(),
+  val deserializationFeatures:List[(DeserializationFeature, Boolean)] = List(),
+  val parserFeatures:List[(JsonParser.Feature, Boolean)] = List()
+) extends JacksLike
+
+object Main {
+  val jacks = new Jacks(parserFeatures = List((JsonParser.Feature.ALLOW_NUMERIC_LEADING_ZEROS, true)))
+
+  def deserialize[T:Manifest](in:String) = {
+    println("in: " + in)
+    val out = jacks.deserialize[T](in)
+    println("out: " + out)
+    out
+  }
+
+  def main(args:Array[String]) {
+    deserialize[Map[String,Int]]("""{ "x": 10 }""")
+    deserialize[Map[String,Int]]("""{ "x": 0010 }""")
+  }
+}
