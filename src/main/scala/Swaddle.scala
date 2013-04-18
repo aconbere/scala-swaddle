75580bf472208ff6411a8e8287281897f2fdd6d5
Anders Conbere <aconbere@etsy.com>
2013-04-18 16:12:50 -0700

    minor edits

--- src/main/scala/Swaddle.scala
+++ src/main/scala/Swaddle.scala
@@ -12,26 +12,24 @@ trait SwaddleLike {
   def deserialize[T: Manifest](value: String) : T =
     mapper.readValue(value, typeReference[T])
 
-  lazy val mapper =  {
+  lazy val mapper = {
     val m = new ObjectMapper()
     m.registerModule(DefaultScalaModule)
     m
   }
 
-  private [this] def typeReference[T: Manifest] = new TypeReference[T] {
+  private def typeReference[T: Manifest] = new TypeReference[T] {
     override def getType = typeFromManifest(manifest[T])
   }
 
-  private [this] def typeFromManifest(m: Manifest[_]): Type = {
-      if (m.typeArguments.isEmpty) {
-        m.runtimeClass 
-      } else new ParameterizedType {
-        def getRawType = m.runtimeClass
-
-        def getActualTypeArguments = m.typeArguments.map(typeFromManifest).toArray
-
-        def getOwnerType = null
-      }
+  private def typeFromManifest(m: Manifest[_]): Type = {
+    if (m.typeArguments.isEmpty) {
+      m.runtimeClass 
+    } else new ParameterizedType {
+      def getRawType = m.runtimeClass
+      def getActualTypeArguments = m.typeArguments.map(typeFromManifest).toArray
+      def getOwnerType = null
+    }
   }
 }
 
